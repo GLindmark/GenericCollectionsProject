57cfa58975564e6d41b1540fe008c70e048b425e
lindmark421@gmail.com <lindmark421@gmail.com>
2019-07-31 14:33:03 -0400

    Generic List

--- GenericCollectionsProject/Program.cs
+++ GenericCollectionsProject/Program.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericCollectionsProject {
     class Program {
         static void Main(string[] args) {
-          
-        }
+
+            List<string> names = new List<string>()
+
+            var integers = new List<int>();
+            integers.Add(7);
+            integers.Add(4);
+            integers.Add(13);
+
+            integers.Remove(4);//by removing the 4, the 13 becomes the second number in the list
+
+            integers.AddRange(new int[] { 4, 44, 444, 4444});
+
+            integers[3] = 99;//this removed the 44 from its position and replaced it with '99'
+
+            foreach(var i in integers) {
+                Console.WriteLine($"i= {i}");
+            }
+        } 
     }
 }
